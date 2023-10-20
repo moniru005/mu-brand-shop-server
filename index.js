3580afa8ebbfee5fb9a22040ec2a5cc9d7864a64
Monir <moniru005@gmail.com>
2023-10-20 14:45:39 +0600

    User

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,6 +25,57 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('brandShopDB').collection('user');
+    const productCollection = client.db('brandShopDB').collection('product')
+
+    //Product API's Data
+
+    app.get('/products', async(req, res) =>{
+      const products = await productCollection.find().toArray();
+      res.send(products);
+    })
+    
+    app.get('/products/:id', async(req, res) =>{
+      const id = req.params.brand;
+      const query = {_id: new ObjectId(id)}
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    })
+
+  
+    
+    app.post('/products', async(req, res) =>{
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+      console.log(result);
+    })
+
+    //User APIs Data
+    app.get('/users', async(req, res) =>{
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    })
+
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/users', async(req, res) =>{
+      const user = req.body;
+      const filter = {email: user.email}
+      const updateDoc = {
+        $set: {
+          lastLoggedAt: user.lastLoggedAt
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -38,7 +89,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Users Management server is running");
+  res.send("Mu Brand Shop server is running");
 });
 
 app.listen(port, () => {
