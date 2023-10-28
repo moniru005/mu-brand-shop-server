3e0ef99af208b54fe048f17c4c5c6e0fba7d58ce
Monir <moniru005@gmail.com>
2023-10-28 21:22:04 +0600

    remove await because of vercel 404 error, please consider for this change because of after submit the assignment few hours later i saw the site 404 error for vercel, then i try to solve from internet but its cannot work. Now i found  another way to solve, thats why i want to try

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
     const userCollection = client.db('brandShopDB').collection('user');
     const productCollection = client.db('brandShopDB').collection('product');
